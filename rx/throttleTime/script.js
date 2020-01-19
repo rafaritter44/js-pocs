3f207d690ed1d4c797e9a8c92f750c4757c9d584
Rafael Ritter <rafaritter44@hotmail.com>
2020-01-18 22:35:09 -0300

    Add one more RxJS throttleTime POC

--- rx/throttleTime/script.js
+++ rx/throttleTime/script.js
@@ -4,3 +4,11 @@ Rx.Observable.fromEvent(button, 'click')
     .map(event => event.clientX)
     .throttleTime(1000)
     .subscribe(value => console.log(value));
+
+var observable = Rx.Observable.interval(1000);
+var observer = value => console.log(value);
+
+observable
+    .map(value => 'Number: ' + value)
+    .throttleTime(1900)
+    .subscribe(observer);
