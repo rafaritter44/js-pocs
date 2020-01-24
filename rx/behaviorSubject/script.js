fbb6d5ced6095060f5a2d1e18be12070914106e1
Rafael Ritter <rafaritter44@hotmail.com>
2020-01-24 20:17:15 -0300

    Implement RxJS BehaviorSubject

---
+++ rx/behaviorSubject/script.js
@@ -0,0 +1,11 @@
+// var clickEmitted = new Rx.Subject();
+var clickEmitted = new Rx.BehaviorSubject('Not clicked');
+
+var button = document.querySelector('button');
+var div = document.querySelector('div');
+
+button.addEventListener('click', () => clickEmitted.next('Clicked!'));
+
+clickEmitted.subscribe(value => div.textContent = value);
+
+// clickEmitted.next('Not clicked');
