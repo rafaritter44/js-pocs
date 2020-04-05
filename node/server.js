0e89bd190ba01ce61743299655c6da2207e8ad9e
Rafael Ritter <rafaritter44@hotmail.com>
2020-04-05 19:36:30 -0300

    Implement pure Node.js server

---
+++ node/server.js
@@ -0,0 +1,46 @@
+const http = require('http');
+
+const people = new Map();
+const counter = (() => {
+  let count = 1;
+  return () => count++;
+})();
+
+http.createServer((request, response) => {
+  if (request.url.startsWith('/api/people')) {
+    if (request.method === 'POST') {
+      request.on('data', chunk => {
+        const person = JSON.parse(chunk);
+        const id = counter();
+        person.id = id;
+        people.set(id, person);
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify(person));
+        response.end();
+      });
+    } else if (request.method === 'GET') {
+      const id = request.url.substring('/api/people/'.length);
+      if (/[0-9]+/.test(id)) {
+        const person = people.get(parseInt(id));
+        if (person) {
+          response.setHeader('Content-Type', 'application/json');
+          response.write(JSON.stringify(person));
+          response.end();
+        } else {
+          response.statusCode = 404;
+          response.end();
+        }
+      } else {
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify(Array.from(people.values())));
+        response.end();
+      }
+    } else {
+      response.statusCode = 405;
+      response.end();
+    }
+  } else {
+    response.statusCode = 404;
+    response.end();
+  }
+}).listen(3000);
